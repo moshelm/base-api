bd0717c49974af97b1de77fec0002635ff8745c0
Moshe <moshe@example.com>
2026-01-12 11:38:02 +0200

    add on eendpoint GET /items

--- main.py
+++ main.py
@@ -8,5 +8,10 @@ app = FastAPI()
 def home_page():
     return {"massege":"wolcome hero"}
 
+
+@app.get("/items")
+def get_items():
+    return {"massege":"you are fucking the best"}
+
 if __name__=="__main__":
     uvicorn.run(app,host="0.0.0.0",port=8000)
